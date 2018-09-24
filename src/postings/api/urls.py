a8b93fd16881ce7b750830c4f70254ca3427a50c
kebsont <mktapha@gmail.com>
2018-09-24 19:24:23 +0200

    update urls in api folder and project

---
+++ src/postings/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import KhassidaPostRudView
+
+
+urlpatterns = [
+    url(r'^$', KhassidaPostAPIView.as_view(), name='post-listcreate'),
+    url(r'^(?P<pk>\d+)/$', KhassidaPostRudView.as_view(), name='post-rud')
+]
