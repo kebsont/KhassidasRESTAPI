1088a173101b16ac23d7e4a04687f1dcae039445
kebsont <mktapha@gmail.com>
2018-09-24 18:56:40 +0200

    add models

--- src/postings/models.py
+++ src/postings/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class KhassidaPost(models.Model):
+    # pk aka id --> numbers
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title       = models.CharField(max_length=120, null=True, blank=True)
+    file        = models.FileField(blank=False,null=False)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
