3ca983ab399530abc9a6e5bd7f29ec16d11c3415
kebsont <mktapha@gmail.com>
2018-10-07 15:47:54 +0200

    push propre branch home

--- src/postings/api/urls.py
+++ src/postings/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import KhassidaPostRudView, KhassidaPostAPIView
 
+app_name = 'postings'
 
 urlpatterns = [
     url(r'^$', KhassidaPostAPIView.as_view(), name='post-listcreate'),
